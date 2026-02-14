e1f622c7cb58e5277f9a30efc2b950d8f658e394
Davidhdy22 <450315151@qq.com>
2026-02-14 19:07:13 +0800

    V1
    original

---
+++ utils.py
@@ -0,0 +1,27 @@
+from prompt_template import system_template_text,user_template_text
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.output_parsers import PydanticOutputParser
+from xiaohongshu_model import Xiaohongshu
+import os
+def generate_xiaohongshu(theme,openai_api_key):
+    prompt= ChatPromptTemplate.from_messages([
+        ("system",system_template_text),
+        ("user",user_template_text)]
+    )
+    model=ChatOpenAI(
+        model="deepseek-chat",
+        openai_api_key=openai_api_key,
+        openai_api_base="https://api.deepseek.com/v1"
+    )
+    output_parser=PydanticOutputParser(pydantic_object=Xiaohongshu)
+    chain=prompt|model|output_parser
+    result=chain.invoke(
+        {
+            "parser_instructions":output_parser.get_format_instructions(),
+            "theme":theme
+        }
+    )
+    return result
+
+print(generate_xiaohongshu("大模型",os.getenv("DEEPSEEK_API_KEY")))
